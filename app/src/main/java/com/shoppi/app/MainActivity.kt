f816beb4715daf7d5630fef7d0e94399ac0cc8e1
sorayeon <jihwan77@gmail.com>
2022-01-20 02:31:32 +0900

    Navigation Controller 적용
    화면간의 이동을 구현 : Jetpack Navigation component
    화면간 이동 시각적으로 : navigation graph
    화면간 이동을 관리하는 객체 : NavController
     (기존에 사용하던 transation 에서 변경)

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,6 +2,8 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
         bottomNavigationView.itemIconTintList = null // icon Tint 기본값을 초기화 (gradient 아이콘사용)
+
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+
+        navController?.let {
+            // NavController -> nav host fragment 에서 이동을 관리
+            bottomNavigationView.setupWithNavController(it) // bottom navigation view 와 fragment container view nav host fragment 를 연결 할 수 있다
+        }
     }
 }
