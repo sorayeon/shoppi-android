193213264ef0ee40384ce645abe221b785267b8d
sorayeon <jihwan77@gmail.com>
2022-01-20 01:22:47 +0900

    Bottom navigation
    Bottom navigation 메뉴 추가
    Vector asset drawable 추가
    선택에 따른 아이콘 on off 표시를 위해 selector drawable 추가
    menu 리소스 추가

--- app/src/main/java/com/shoppi/app/MainActivity.kt
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -2,10 +2,14 @@ package com.shoppi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null // icon Tint 기본값을 초기화 (gradient 아이콘사용)
     }
 }
