02840d3d2243cc18a529e7ccc9f6f7249bac2ab0
sorayeon <jihwan77@gmail.com>
2022-02-12 19:31:51 +0900

    HomeData DataClass 구성
    HemeData
     - Title : Top AppBar 타이틀 영역
     - List<Banner> : 이미지 배너 목록

---
+++ app/src/main/java/com/shoppi/app/model/Banner.kt
@@ -0,0 +1,26 @@
+package com.shoppi.app.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Banner(
+    @SerializedName("background_image_url") val backgroundImageUrl: String,
+    val badge: BannerBadge,
+    val label: String,
+    @SerializedName("product_detail") val productDetail: BannerProductDetail,
+)
+
+data class BannerBadge(
+    val label: String,
+    @SerializedName("background_color") val backgroundColor: String,
+)
+
+data class BannerProductDetail(
+    @SerializedName("brand_name") val brandName: String,
+    val label: String,
+    @SerializedName("discount_rate") val discountRate: Int,
+    val price: Int,
+    @SerializedName("thumbnail_image_url") val thumbnailImageUrl: String,
+    @SerializedName("product_id") val productId: String,
+)
+
+
--- app/src/main/java/com/shoppi/app/model/HomeData.kt
+++ app/src/main/java/com/shoppi/app/model/HomeData.kt
@@ -6,29 +6,3 @@ data class HomeData(
     val title: Title,
     @SerializedName("top_banners") val topBanners: List<Banner>
 )
-
-data class Banner(
-    @SerializedName("background_image_url") val backgroundImageUrl: String,
-    val badge: BannerBadge,
-    val label: String,
-    @SerializedName("product_detail") val productDetail: BannerProductDetail
-)
-
-data class BannerBadge(
-    val label: String,
-    @SerializedName("background_color") val backgroundColor: String
-)
-
-data class BannerProductDetail(
-    @SerializedName("brand_name") val brandName: String,
-    val label: String,
-    @SerializedName("discount_rate") val discountRate: Int,
-    val price: Int,
-    @SerializedName("thumbnail_image_url") val thumbnailImageUrl: String,
-    @SerializedName("product_id") val productId: String
-)
-
-data class Title(
-    val text: String,
-    @SerializedName("icon_url") val iconUrl: String
-)
